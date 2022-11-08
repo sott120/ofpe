6f850d8de7c4c6fcc9cb013623cc4b949e6cb55e
Soyeon Park <psy_sott120@naver.com>
2022-11-09 03:06:19 +0900

    로그인화면 제작중

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import FloatingLabel from "react-bootstrap/FloatingLabel";
+import { Button, Form, FloatingLabel } from "react-bootstrap";
 import styled from "styled-components";
 import axios from "axios";
 import { useState } from 'react';
@@ -15,9 +13,13 @@ let LoginBox = styled.section`
     max-width: 500px;
     width: 100%;
     padding: 50px 20px;
+    margin: 0 20px;
     box-sizing: border-box;
-    background-color: rgba(0,0,0,50%);
+    background-color: rgba(0, 0, 0, 50%);
     text-align: center;
+    &div {
+        color: white;
+    }
 `;
 
 interface CardInterface {
@@ -36,56 +38,67 @@ const Card = styled.div<CardInterface>`
     }
 `;
 
+
 const LoginWrap = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
+    background-image: url('/image/login_bg.jpg');
+    background-size: cover;
+    background-position: center center;
+    background-repeat: no-repeat;
 `;
 
-const StForm = styled(Form)<CardInterface>`
-    background-color: red;
-    width: ${(props) => props.width}px;
+const FormCheck = styled(Form.Check)<CardInterface>`
+    display: inline-block;
 `;
 
 
 
 const Login = () => {
-    return(
+    return (
         <LoginWrap>
-          {/* <Card width={500}></Card> */}
-          {/* <Card width={500} className={act ? 'active' : undefined}></Card> */}
-          <LoginBox>
-              <h1>OFPE</h1>
-              <h3>우리의 필름 사진전</h3>
-              <StForm>
-                  <FloatingLabel
-                      controlId="floatingInput"
-                      label="아이디"
-                      className="mb-3"
-                  >
-                      <Form.Control type="id" placeholder="아이디" />
-                  </FloatingLabel>
-                  <FloatingLabel controlId="floatingPassword" label="비밀번호">
-                      <Form.Control type="password" placeholder="비밀번호" />
-                  </FloatingLabel>
-                  <Form.Text className="text-muted">
-                      비밀번호가 일치하지 않습니다.ㅁ
-                  </Form.Text>
-                  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                      <Form.Check
-                          type="checkbox"
-                          label="다음부턴 자동 로그인"
-                      />
-                  </Form.Group>
-                  <Button variant="primary" type="submit">
-                      로그인
-                  </Button>
-              </StForm>
-          </LoginBox>
-           </LoginWrap>
-    )
+            <LoginBox>
+                <h1>OFPE</h1>
+                <h3>우리의 필름 사진전</h3>
+                <Form>
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label="아이디"
+                        className="mb-3"
+                    >
+                        <Form.Control type="id" placeholder="아이디" />
+                    </FloatingLabel>
+                    <Form.Text className="text-muted">
+                        아이디를 입력해주세요.
+                    </Form.Text>
+                    <FloatingLabel
+                        controlId="floatingPassword"
+                        label="비밀번호"
+                    >
+                        <Form.Control type="password" placeholder="비밀번호" />
+                    </FloatingLabel>
+                    <Form.Text className="text-muted">
+                        비밀번호가 일치하지 않습니다.
+                    </Form.Text>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <FormCheck
+                            type="checkbox"
+                            label="다음부턴 자동 로그인"
+                        />
+                    </Form.Group>
+                    <Button variant="dark" type="submit">
+                        로그인
+                    </Button>
+                </Form>
+                <Button variant="warning" type="submit">
+                    게스트로 로그인
+                </Button>
+            </LoginBox>
+        </LoginWrap>
+    );
 }
 
 function App() {
@@ -96,7 +109,7 @@ function App() {
       <>
           <Login />
 
-          <button
+          {/* <button
               onClick={() => {
                 SetAct(()=> !act)
                   axios
@@ -131,7 +144,7 @@ function App() {
               }}
           >
               TEST2
-          </button>
+          </button> */}
       </>
   );
 
