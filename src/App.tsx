756225be197572c981016696a88c584967bab2f7
Soyeon Park <psy_sott120@naver.com>
2022-11-10 21:54:33 +0900

    masonry 추가, 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,166 +1,138 @@
-// import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Form, FloatingLabel } from "react-bootstrap";
+import { Container, Nav, Navbar, Button, Row, Col } from "react-bootstrap";
 import styled from "styled-components";
 import GlobalStyles from "./GlobalStyles";
 import axios from "axios";
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import {Login, Join} from './Pages/LoginJoin';
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Masonry from "react-masonry-css";
+import { prototype } from "stream";
 
-const ip = process.env.REACT_APP_ip;
+const breakpointColumnsObj = {
+    default: 4,
+    1199: 3,
+    767: 2,
+    500: 1,
+};
 
-const LoginWrap = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-image: url("/image/login_bg.jpg");
-    background-size: cover;
-    background-position: center center;
-    background-repeat: no-repeat;
-`;
-
-interface LoginBoxItf {
-    boxOpacity?: number;
-    textAlign?: string;
-}
-
-const LoginBox = styled.section<LoginBoxItf>`
-    max-width: 500px;
-    width: 100%;
-    padding: 60px 30px;
-    margin: 0 20px;
-    box-sizing: border-box;
-    background-color: rgba(0, 0, 0, ${(props) => props.boxOpacity}%);
-    text-align: ${(props) => props.textAlign};
-    color: white;
-`;
-
-interface CardInterface {
-    width:number;
-}
-
-const Card = styled.div<CardInterface>`
-    width: ${(props) => props.width}px;
-    height: 100px;
-    background-color: green;
-    &:hover {
-        background-color: red;
+const CustomMasonry = styled(Masonry)`
+    margin-top: 1.5rem;
+    &.my-masonry-grid {
+        display: -webkit-box; /* Not needed if autoprefixing */
+        display: -ms-flexbox; /* Not needed if autoprefixing */
+        display: flex;
+        width: auto;
     }
-    &.active {
-        background-color: yellow;
+    &.my-masonry-grid_column {
+        /* gutter size */
+        background-clip: padding-box;
+    }
+`;
+
+const Figure = styled.div`
+    padding: 10px;
+    position: relative;
+    & img {
+        width: 100%;
+    }
+    & figcaption {
+        position: absolute;
+        bottom: 20px;
+        right: 20px;
+        color: yellow;
     }
 `;
 
 
 
-
-const FormCheck = styled(Form.Check)<CardInterface>`
-    display: inline-block;
-`;
-
-
-
-const Login = ({ boxOpacity, textAlign }: LoginBoxItf) => {
+const Main = () => {
+    let navigate = useNavigate();
     return (
-        <LoginWrap>
-            <LoginBox boxOpacity={60} textAlign={"center"}>
-                <h1 className="mb-3">OFPE</h1>
-                <h3 className="mb-5">우리들의 필름 사진전</h3>
-                <Form>
-                    <FloatingLabel
-                        controlId="floatingInput"
-                        label="아이디"
-                        className="mb-3 text-dark"
+        <>
+            <Navbar expand="sm" bg="dark" variant="dark" sticky="top">
+                <Container>
+                    <Navbar.Brand
+                        onClick={() => {
+                            navigate("/");
+                        }}
                     >
-                        <Form.Control 
-                            type="text"
-                        />
-                        <Form.Text className="invalid-feedback">
-                            아이디를 입력해주세요.
-                        </Form.Text>
-                    </FloatingLabel>
+                        OFPE
+                    </Navbar.Brand>
 
-                    <FloatingLabel
-                        controlId="floatingPassword"
-                        label="비밀번호"
-                        className="mb-3 text-dark"
-                    >
-                        <Form.Control type="password" placeholder="비밀번호" />
-                        <Form.Text className="invalid-feedback">
-                            비밀번호가 일치하지 않습니다.
-                        </Form.Text>
-                    </FloatingLabel>
-
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <FormCheck
-                            type="checkbox"
-                            label="다음부턴 자동 로그인"
-                        />
-                    </Form.Group>
-                    <Button variant="warning" type="submit" className="mb-3">
-                        로그인
-                    </Button>
-                </Form>
-                <Button variant="dark" type="submit">
-                    게스트로 로그인
-                </Button>
-            </LoginBox>
-        </LoginWrap>
+                    <Nav className="me-auto">
+                        {/* <Nav.Link onClick={()=>{navigate(-1)}}>뒤로가기</Nav.Link> */}
+                        {/* <Link className="nav_link" to="/">Home</Link>
+                        <Link className="nav_link" to="/detail">Detail</Link> */}
+                    </Nav>
+                    <Nav>
+                        {/* <Nav.Link
+                            onClick={() => {
+                                navigate("/");
+                            }}
+                        >
+                            박소연
+                        </Nav.Link>
+                        <Nav.Link disabled>
+                            님, 오늘의 사진은 어떠셨나요?
+                        </Nav.Link>
+                        */}
+                        <Button
+                            className="ms-3"
+                            onClick={() => {
+                                navigate("/");
+                            }}
+                            variant="warning"
+                        >
+                            찰칵!
+                        </Button>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <Container>
+                <CustomMasonry
+                    className="my-masonry-grid"
+                    columnClassName="my-masonry-grid_column"
+                    breakpointCols={breakpointColumnsObj}
+                >
+                    <Figure>
+                        <img src="/image/login_bg.jpg" alt="" />
+                        <figcaption>1번</figcaption>
+                    </Figure>
+                    <Figure>
+                        <img src="/image/login_bg2.jpg" alt="" />
+                        <figcaption>2</figcaption>
+                    </Figure>
+                    <Figure>
+                        <img src="/image/login_bg.jpg" alt="" />
+                        <figcaption>3</figcaption>
+                    </Figure>
+                    <Figure>
+                        <img src="/image/login_bg2.jpg" alt="" />
+                        <figcaption>4</figcaption>
+                    </Figure>
+                    <Figure>
+                        <img src="/image/login_bg2.jpg" alt="" />
+                        <figcaption>5</figcaption>
+                    </Figure>
+                    <Figure>
+                        <img src="/image/login_bg.jpg" alt="" />
+                        <figcaption>6</figcaption>
+                    </Figure>
+                    <Figure>
+                        <img src="/image/login_bg.jpg" alt="" />
+                        <figcaption>7</figcaption>
+                    </Figure>
+                    <Figure>
+                        <img src="/image/login_bg.jpg" alt="" />
+                        <figcaption>8</figcaption>
+                    </Figure>
+                </CustomMasonry>
+            </Container>
+        </>
     );
-};
-
-
-
-const Join = ({ boxOpacity, textAlign }: LoginBoxItf) => {
-
-    return (
-        <LoginWrap>
-            <LoginBox boxOpacity={100} textAlign={"left"}>
-                <h1 className="mb-3">OFPE</h1>
-                <h3 className="mb-5">회원가입</h3>
-                <Form>
-                    <Form.Group className="mb-3">
-                        <Form.Label>아이디</Form.Label>
-                        <Form.Control name="userName" onChange={handleInput} type="text" />
-                        <Form.Text className="invalid-feedback">
-                            아이디를 입력해주세요
-                        </Form.Text>
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>닉네임</Form.Label>
-                        <Form.Control type="text" />
-                        <Form.Text className="invalid-feedback">
-                            닉네임을 입력해주세요
-                        </Form.Text>
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>비밀번호</Form.Label>
-                        <Form.Control type="text" />
-                        <Form.Text className="invalid-feedback">
-                            비밀번호를 입력해주세요.
-                        </Form.Text>
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>비밀번호 확인</Form.Label>
-                        <Form.Control type="text" />
-                        <Form.Text className="invalid-feedback">
-                            비밀번호를 확인해주세요
-                        </Form.Text>
-                    </Form.Group>
-                    <Button variant="warning" type="submit" className="mb-3">
-                        회원가입
-                    </Button>
-                </Form>
-                <Button variant="dark" type="submit">
-                    게스트로 보기
-                </Button>
-            </LoginBox>
-        </LoginWrap>
-    );
-};
+    
+}
 
 function App() {
 
@@ -169,9 +141,9 @@ function App() {
   return (
         <>
             <GlobalStyles />
-            <Login/>
-            <Join />
-
+            {/* <Login/>
+            <Join /> */}
+            <Main />
             {/* <button
                 onClick={() => {
                   SetAct(()=> !act)
