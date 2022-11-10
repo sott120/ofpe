793de9827333f74dcbf1b95ae44ac07a48bdfbb5
Soyeon Park <psy_sott120@naver.com>
2022-11-11 04:15:55 +0900

    제목태그 font-size 추가

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -3,11 +3,17 @@ import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle` 
 ${reset};
-.h1, h1 {
-    font-size: calc(1.375rem + 1.5vw);
+h1, .h1 {
+  font-size: calc(1.375rem + 1.5vw) ;
 }
-.h3, h3 {
-    font-size: calc(1.3rem + .6vw);
+h2, .h2 {
+  font-size: calc(1.325rem + 0.9vw) ;
+}
+h3, .h3 {
+  font-size: calc(1.3rem + 0.6vw) ;
+}
+h4, .h4 {
+  font-size: calc(1.275rem + 0.3vw) ;
 }
 .label, label{
     vertical-align: sub;
