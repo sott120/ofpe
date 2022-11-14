548c719e7ba3711f274fa792982553122f8579bf
Soyeon Park <psy_sott120@naver.com>
2022-11-14 23:57:03 +0900

    헤더 로고 추가

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -7,23 +7,46 @@ import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import Masonry from "react-masonry-css";
 import { prototype } from "stream";
 
+const NavbarBrand = styled(Navbar.Brand)`
+    cursor: pointer;
+    position: relative;
+    & div {
+        position: absolute;
+        width: 54px;
+        height: 54px;
+        top: -22px;
+        left: 0;
+    }
+    & div::after {
+        content: "OFPE";
+        margin-left: 10px;
+        position: absolute;
+        top: 17px;
+    }
+    & div > img {
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+const NavLogo = styled.img`
+    
+`
+
 const Header = () => {
     let navigate = useNavigate();
     return (
         <Navbar expand="sm" bg="dark" variant="dark" sticky="top">
             <Container className="ps-4 pe-4">
-                <Navbar.Brand
+                <NavbarBrand
                     onClick={() => {
                         navigate("/");
                     }}
                 >
-                    OFPE
-                </Navbar.Brand>
-                <Nav className="me-auto">
-                    {/* <Nav.Link onClick={()=>{navigate(-1)}}>뒤로가기</Nav.Link> */}
-                    {/* <Link className="nav_link" to="/">Home</Link>
-                        <Link className="nav_link" to="/detail">Detail</Link> */}
-                </Nav>
+                    <div>
+                        <img src="./image/ofpe_logo.png" alt="" />
+                    </div>
+                </NavbarBrand>
                 <Nav>
                     {/* <Nav.Link
                             onClick={() => {
