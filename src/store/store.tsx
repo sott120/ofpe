a6e994a1260951dbd9b0197552b5bfc0fcf9b3bd
Soyeon Park <psy_sott120@naver.com>
2022-12-06 23:59:15 +0900

    폴더 정리

---
+++ src/store/store.tsx
@@ -0,0 +1,13 @@
+import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import userSlice from './userSlice';
+
+export const store = configureStore({
+  reducer: userSlice,
+});
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -17,9 +17,14 @@ export const userLogin = createSlice({
       state.user.id = action.payload[0].id;
       state.user.name = action.payload[0].name;
     },
+    replaceUser(state: stateType, action) {
+      console.log(action);
+      state.user.id = action.payload.id;
+      state.user.name = action.payload.name;
+    },
   },
   extraReducers: {},
 });
-export const { setUser } = userLogin.actions;
+export const { setUser, replaceUser } = userLogin.actions;
 
 export default userLogin.reducer;
