bf33e337f83889589fd86d561ed5169520c3b92c
Soyeon Park <psy_sott120@naver.com>
2022-12-07 23:54:52 +0900

    토큰 에러 처리

--- src/util/cookieErr.tsx
+++ src/util/cookieErr.tsx
@@ -1,5 +1,9 @@
-const cookieErr = (errText: string) => {
-  alert(errText);
+const cookieErr = (errNum: number) => {
+  if (errNum === 401) {
+    alert('로그인 후 이용해주세요.');
+  } else if (errNum === 403) {
+    alert('토큰이 만료되었습니다. 다시 로그인해주세요.');
+  }
   window.location.replace('/login');
 };
 
