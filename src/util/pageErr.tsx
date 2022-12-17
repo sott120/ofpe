4b6021ea4e6a2d2e2eb9aec8aae7dd3d18c6db57
Soyeon Park <psy_sott120@naver.com>
2022-12-18 01:13:24 +0900

    에러 페이지 경로 변경

--- src/util/pageErr.tsx
+++ src/util/pageErr.tsx
@@ -10,7 +10,7 @@ const cookieErr = (errNum: number) => {
 const pageErr = (chkId: string | undefined) => {
   if (chkId) {
     alert('이미 로그인 되어있습니다!');
-    window.history.back();
+    window.location.replace('/');
   }
 };
 
